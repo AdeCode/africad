d5493513aed90f12909c4b827b3b3f6d269ee809
adecode <agorohabeeb@gmail.com>
2022-01-19 14:02:36 +0100

    modified index file

--- pages/index.vue
+++ pages/index.vue
@@ -41,29 +41,8 @@
                   required
                 ></b-form-input>
               </b-form-group>
-
-              <!-- <b-form-group id="input-group-3" label="Food:" label-for="input-3">
-                <b-form-select
-                  id="input-3"
-                  v-model="form.food"
-                  :options="foods"
-                  required
-                ></b-form-select>
-              </b-form-group> -->
-
-              <!-- <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-                <b-form-checkbox-group
-                  v-model="form.checked"
-                  id="checkboxes-4"
-                  :aria-describedby="ariaDescribedby"
-                >
-                  <b-form-checkbox value="me">Check me out</b-form-checkbox>
-                  <b-form-checkbox value="that">Check that out</b-form-checkbox>
-                </b-form-checkbox-group>
-              </b-form-group> -->
-
+             
               <b-button type="submit" variant="primary">Join Our Waitlist</b-button>
-              <!-- <b-button type="reset" variant="danger">Reset</b-button> -->
             </b-form>   
 
             <div class="form-footer">
@@ -85,6 +64,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'IndexPage',
 
@@ -95,13 +75,13 @@ export default {
           email: '',
           name: '',         
       },
-      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
       show: true
     }
   },
   methods: {
     onSubmit(event) {
       event.preventDefault()
+
       alert(JSON.stringify(this.form))
       console.log(this.form)
     },
@@ -110,8 +90,7 @@ export default {
       // Reset our form values
       this.form.email = ''
       this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
+     
       // Trick to reset/clear native browser form validation state
       this.show = false
       this.$nextTick(() => {
